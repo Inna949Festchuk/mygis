d184f18473d551ee06ee75e6469246a47333830a
Inna949Festchuk <innabori@mail.ru>
2025-05-21 03:47:28 +0200

    v0.0.1 add control

--- gisapp/views.py
+++ gisapp/views.py
@@ -8,7 +8,7 @@ from django.core.serializers import serialize
 def get_context_data(request):
     data_geojson = serialize('geojson', ValuesPoints.objects.all(),
             geometry_field='geom',
-            fields=('f3'))
+            fields=('f3',)) # поле по которому производится построение тепловой карты (на фронте)
     
     valid_data = json.loads(data_geojson)
     
