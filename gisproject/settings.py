dd1900d352ffbcbaf76830d3e3b000dc9bb77748
Inna949Festchuk <innabori@mail.ru>
2025-05-27 20:16:32 +0200

    v0.0.3 add env

--- gisproject/settings.py
+++ gisproject/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
+
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,16 +25,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)1by=-2*)fpz9p4t6h)r10=jo_3s0my&#u*j3&+kt9h@ciy+mo'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = [
+    os.getenv('ALLOWED_HOSTS'),
+]
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -73,23 +78,15 @@ WSGI_APPLICATION = 'gisproject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'mygis',
-        'USER': 'postgres',
-        'PASSWORD': 'mygispass',
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
         # 'HOST' : 'localhost', 
-        'HOST': 'db',  # Имя службы PostgreSQL в docker-compose
-        'PORT': '5432',
+        'HOST': os.getenv('DB_HOST'),  # Имя службы PostgreSQL в docker-compose
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
